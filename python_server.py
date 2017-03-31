01c0a0f21a42ef7b494146e3fbf9b433802903ff
Andrew Prosikhin <andrewpros@mail.ru>
2017-03-30 22:01:01 -0400

    Working simple Python server.

---
+++ python_server.py
@@ -0,0 +1,24 @@
+import time
+import BaseHTTPServer
+  
+
+HOST_NAME = 'localhost'
+PORT_NUMBER = 8000
+  
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+      def do_GET(s):
+         s.send_response(200)
+         s.send_header("Content-type", "text/html")
+         s.end_headers()
+         s.wfile.write("<html><head><title>Title goes here.</title></head>")
+         s.wfile.write("<body><p>This is a test.</p>")
+         # If someone went to "http://something.somewhere.net/foo/bar/",
+         # then s.path equals "/foo/bar/".
+         s.wfile.write("<p>You accessed path: %s</p>" % s.path)
+         s.wfile.write("</body></html>")
+
+if __name__ == '__main__':
+   server_class = BaseHTTPServer.HTTPServer
+   httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+   print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+   httpd.serve_forever()
