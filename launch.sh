59927851c3760c2c575bc72e70907791ee7b1fe3
Andrew Prosikhin <andrewpros@mail.ru>
2017-03-31 21:56:38 -0400

    Elasticsearch container added, its launch automated. Network created.

--- launch.sh
+++ launch.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+docker network create elk-network
 docker build -t docker_elk/server_img -f server_build.df .
-docker run -d -p 8000:8000 docker_elk/server_img 
+docker run -d -p 8000:8000 --name docker_elk_server --net=elk-network docker_elk/server_img
+docker run -d -p 9200:9200 --name docker_elk_es --net=elk-network elasticsearch:5.1 
