2efbaeaf251088367278ec5260e96f29582882ae
Andrew Prosikhin <andrewpros@mail.ru>
2017-04-04 19:01:08 -0400

    Not clear if logstash connects, but ES container is crashing.

--- launch.sh
+++ launch.sh
@@ -4,4 +4,4 @@ docker network create elk-network
 docker build -t docker_elk/server_img -f server_build.df .
 docker run -d -p 8000:8000 --name docker_elk_server --net=elk-network docker_elk/server_img
 docker run -d -p 9200:9200 --name docker_elk_es --net=elk-network elasticsearch:5.1
-docker run -it --net=elk-network -v /Users/iOS_Coder/DockerELK/pipeline/:/usr/share/logstash/pipeline/ docker.elastic.co/logstash/logstash:5.2.0
+docker run -it --net=elk-network -v /Users/iOS_Coder/DockerELK/pipeline/:/usr/share/logstash/pipeline/ docker.elastic.co/logstash/logstash:5.1.2 
