e5bbc9ca045a9f6cc842a267b669a79381632f3d
aprosikhin <aprosikhin@freewheel.tv>
2017-04-06 12:00:16 -0400

    Kibana container connecting to elasticsearch. Launch automated!

--- launch.sh
+++ launch.sh
@@ -5,4 +5,5 @@ docker build -t docker_elk/server_img -f server_build.df .
 docker build -t docker_elk/logstash_img -f logstash_build.df .
 docker run -d --name docker_elk_server --net=elk-network docker_elk/server_img
 docker run -d -p 9200:9200 --name elasticsearch --net=elk-network elasticsearch:5.1
-docker run -d --name logstash --net=elk-network docker_elk/logstash_img 
+docker run -d --name logstash --net=elk-network docker_elk/logstash_img
+docker run -d -p 5601:5601 --name kibana --net=elk-network -e ELASTICSEARCH_URL=http://elasticsearch:9200 kibana:5.1 
